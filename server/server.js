30e2a5883033840acd6f3a5cca44ea957e3a9561
Giovanni Flores <geflores5@gmail.com>
2020-11-12 20:53:00 -0800

    Implement get request for related endpoint call

--- server/server.js
+++ server/server.js
@@ -4,11 +4,22 @@ const morgan = require('morgan');
 const path = require('path');
 
 const db = require('../database');
+const Game = require('../database/Game');
 
 const server = express();
 
 server.use(morgan('dev'));
 server.use(bodyParser.json());
 server.use(express.static(path.join(__dirname, '../client/dist')));
+
+server.use('/api/games/:id/related', (req, res) => {
+  Game.find({}).limit(10)
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
 
 module.exports = server;
