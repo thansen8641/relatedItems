15c096d29903a9a36154aa0bc86fc3b607246de6
Giovanni Flores <geflores5@gmail.com>
2020-11-16 21:47:56 -0800

    Implement responsive design for similar games module, media queries for horizontal scroll and arrows

--- client/src/components/SimilarList.jsx
+++ client/src/components/SimilarList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import SimilarListItem from './SimilarListItem.jsx'
+import SimilarListItem from './SimilarListItem.jsx';
+import '../../dist/style.css';
 
 class SimilarList extends React.Component {
   constructor(props) {
@@ -32,13 +33,13 @@ class SimilarList extends React.Component {
       <div style={{ textAlign: "center", width: "100%" }}>
         <h2>Similar Games</h2>
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%", margin: "0px auto"  }}>
-          {this.state.translate !== 0 ? <button onClick={this.handleLeftArrowClick} style={{ display: "inline-block" }}>&#60;</button> : <div></div> }
-          <div style={{ overflow: "hidden", whiteSpace: "nowrap", display: "inline-block", maxHeight: "300px", width: "100%" }} >
+          {this.state.translate !== 0 ? <button className="similarArrow" onClick={this.handleLeftArrowClick} style={{ display: "inline-block" }}>&#60;</button> : <div></div> }
+          <div className="similar" style={{ whiteSpace: "nowrap", display: "inline-block", maxHeight: "300px", width: "100%", maxWidth: "1200px" }} >
             {this.props.games.map((game) => (
               <SimilarListItem transformStr={transformStr} game={game} />
             ))}
           </div>
-            {this.state.translate !== 1500 ? <button onClick={this.handleRightArrowClick} style={{ display: "inline-block" }}>&#62;</button> : <div></div> }
+            {this.state.translate !== 1500 ? <button className="similarArrow" onClick={this.handleRightArrowClick} style={{ display: "inline-block" }}>&#62;</button> : <div></div> }
         </div>
       </div>
     );
