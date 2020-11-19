adb67a5d9a4378666b1fe1a0643dbf316c8c883f
Giovanni Flores <geflores5@gmail.com>
2020-11-18 18:18:54 -0800

    Minor css adjustments

--- client/src/components/Together.jsx
+++ client/src/components/Together.jsx
@@ -80,48 +80,48 @@ class Together extends React.Component {
   }
 
   render() {
-    const gameStyle = { display: "inline-block", width: "150px", height: "100%"};
+    const gameStyle = { borderRadius: "1%", backgroundColor: "rgb(255, 255, 255)", display: "inline-block", padding: "15px 5px", width: "150px", height: "100%"};
     const nameStyle = { height: "23%", textAlign: "center", textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap" };
 
     return (
-      <div style={{ display: "block", margin: "20px auto", width: "100%" }}>
+      <div style={{ backgroundColor: "rgb(247, 247, 247)", display: "block", margin: "20px auto", width: "100%" }}>
         <div style={{ display: "flex", flexFlow: "wrap", justifyContent: "center", alignItems: "center", textAlign: "center", maxWidth: "900px", width: "100%", margin: "0px auto" }}>
           <h2 style={{ width: "100%", textAlign: "center"}}>Frequently bought together</h2>
           <div>
             <div style={gameStyle}>
-              <div style={{ height: "67%"}}>
+              <div>
                 <img src={this.props.games[0].imgUrl} style={{ width: "100%" }} />
               </div>
-              <p style={{ textAlign: "center" }}>{this.props.games[0].price}</p>
+              <p style={{ fontWeight: "bold", textAlign: "center" }}>{this.props.games[0].price}</p>
               <p style={nameStyle}>{this.props.games[0].item}</p>
               <input type="checkbox" name="zero" onChange={this.changeHandler} checked={this.state.gameZeroChecked} ></input>
             </div>
-            <div style={{ display: "inline-block", transform: "translate(0px, -150px)", fontSize: "36px" }}>&#43;</div>
+            <div style={{ display: "inline-block", margin: "0px 10px", transform: "translate(0px, -150px)", fontSize: "36px" }}>&#43;</div>
             <div style={gameStyle}>
-              <div style={{ height: "67%"}}>
+              <div>
                 <img src={this.props.games[1].imgUrl} style={{ width: "100%" }} />
               </div>
-              <p style={{ height: "15%", textAlign: "center" }}>{this.props.games[1].price}</p>
+              <p style={{ fontWeight: "bold", textAlign: "center" }}>{this.props.games[1].price}</p>
               <p style={nameStyle}>{this.props.games[1].item}</p>
               <input type="checkbox" name="one" onChange={this.changeHandler} checked={this.state.gameOneChecked} ></input>
             </div>
-            <div style={{ display: "inline-block", transform: "translate(0px, -150px)", fontSize: "36px" }} >&#43;</div>
+            <div style={{ display: "inline-block", margin: "0px 10px", transform: "translate(0px, -150px)", fontSize: "36px" }} >&#43;</div>
             <div style={gameStyle}>
-              <div style={{ height: "67%"}}>
+              <div>
                 <img src={this.props.games[2].imgUrl} style={{ width: "100%" }} />
               </div>
-              <p style={{ height: "15%", textAlign: "center" }}>{this.props.games[2].price}</p>
+              <p style={{ fontWeight: "bold", textAlign: "center" }}>{this.props.games[2].price}</p>
               <p style={nameStyle}>{this.props.games[2].item}</p>
               <input type="checkbox" name="two" onChange={this.changeHandler} checked={this.state.gameTwoChecked} ></input>
             </div>
           </div>
         </div>
-        <div style={{ height: "15%", textAlign: "center", maxWidth: "900px", margin: "10px auto"  }}>
+        <div style={{ textAlign: "center", maxWidth: "900px", margin: "10px auto"  }}>
           {
             this.state.totalChecked !== 0 ?
               <div style={{ display: "flex", justifyContent: "center" }}>
                 <p>Subtotal: ${this.state.total} ({this.state.totalChecked} item(s))</p>
-                <button style={{ margin: "15px"}}>
+                <button style={{ borderRadius: "5%", backgroundColor: "rgb(204, 0, 0)", color: "rgb(255, 255, 255)", border: "0px", padding: "10px 5px", margin: "5px 5px"}}>
                   Add {this.state.phrase}to cart
                 </button>
               </div>
